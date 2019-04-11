e8191d5e95438a2c970f81d2138c6fb37648563a
Jason Fu <jasonguangfu@gmail.com>
2019-04-11 13:34:24 +0800

    styled meal save and added post functionality

--- pages/meals/save/save.js
+++ pages/meals/save/save.js
@@ -6,7 +6,7 @@ Page({
    * Page initial data
    */
   data: {
-
+    quantity: 1
   },
 
   /**
@@ -20,7 +20,10 @@ Page({
       method: 'GET',
       success(res) {
         const dish = res.data;
-        page.setData(dish)
+        page.setData({
+          dish: dish
+        });
+        page.setTotal();
         console.log(11, dish)
       }
     })
@@ -75,25 +78,94 @@ Page({
 
   },
 
+  addAmount() {
+    const page = this;
+    let data2 = page.data
+    data2.quantity += 1
+    page.setData(data2)
+    page.setTotal();
+    console.log(page.data.dishes);
+  },
+
+  subtractAmount(e) {
+    const page = this;
+    let data2 = page.data
+    data2.quantity -= 1
+    page.setData(data2)
+    page.setTotal();
+    console.log(page.data.dishes);
+  },
+
+  setTotal() {
+    const page = this;
+    const dish = page.data.dish;
+    let carb = dish.nutrients[0].amount * page.data.quantity;
+    let calorie = dish.nutrients[1].amount * page.data.quantity;
+    let total_fat = dish.nutrients[2].amount * page.data.quantity;
+    let sat_fat = dish.nutrients[3].amount * page.data.quantity;
+    let cholesterol = dish.nutrients[4].amount * page.data.quantity;
+    let sodium = dish.nutrients[5].amount * page.data.quantity;
+    let fiber = dish.nutrients[6].amount * page.data.quantity;
+    let sugar = dish.nutrients[7].amount * page.data.quantity;
+    let protein = dish.nutrients[8].amount * page.data.quantity;
+
+    page.setData({
+      carb: carb,
+      calorie: calorie,
+      total_fat: total_fat,
+      sat_fat: sat_fat,
+      cholesterol: cholesterol,
+      sodium: sodium,
+      fiber: fiber,
+      sugar: sugar,
+      protein: protein
+    });
+  },
+
   addToMeal(e) {
     console.log(e)
+    const page = this;
     const url = app.globalData.url;
-
-    let dish_id = e.currentTarget.dataset.id;
     let user_id = app.globalData.userId;
 
-    let meal = { quantity: 1, user_id: user_id, dish_id: dish_id };
+    // page.data.dishes.forEach((dish) => {
+      let dish_id = page.data.dish.id;
+      let quantity = page.data.quantity
+      let meal = { quantity: quantity, user_id: user_id, dish_id: dish_id }
 
-    wx.request({
-      url: `${url}meals`,
-      method: "POST",
-      data: meal,
-      success(res) {
-        console.log(res)
-        wx.switchTab({
-          url: `/pages/meals/index/index?user_id=${user_id}`
-        })
-      }
-    })
+      wx.request({
+        url: `${url}meals`,
+        method: "POST",
+        data: meal,
+        success(res) {
+          console.log(res);
+          wx.switchTab({
+            url: `/pages/meals/index/index?user_id=${user_id}`
+          });
+        }
+      })
+    // });
   },
+
+  // addToMeal(e) {
+  //   console.log(e)
+  //   const url = app.globalData.url;
+
+  //   let dish_id = e.currentTarget.dataset.id;
+  //   let user_id = app.globalData.userId;
+
+  //   let meal = { quantity: 1, user_id: user_id, dish_id: dish_id };
+
+  //   wx.request({
+  //     url: `${url}meals`,
+  //     method: "POST",
+  //     data: meal,
+  //     success(res) {
+  //       console.log(res)
+  //       wx.switchTab({
+  //         url: `/pages/meals/index/index?user_id=${user_id}`
+  //       })
+  //     }
+  //   })
+  // },
 })
