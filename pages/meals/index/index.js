90c6640fe34535aca49a0b6e490d478807ecc6fa
Michael Ulryck <michael.ulryck@gmail.com>
2019-04-11 10:39:50 +0800

    chart

--- pages/meals/index/index.js
+++ pages/meals/index/index.js
@@ -44,54 +44,54 @@ Page({
   /**
    * Page initial data
    */
-  data: {
-    showInput: function () {
-      this.setData({
-        inputShowed: true
-      });
-    },
-    hideInput: function () {
-      this.setData({
-        inputVal: "",
-        inputShowed: false
-      });
-    },
-    clearInput: function () {
-      this.setData({
-        inputVal: ""
-      });
-    },
-    inputTyping: function (e) {
-      this.setData({
-        inputVal: e.detail.value
-      });
-    },
-
-    upper: function (e) {
-      console.log(e)
-    },
-    lower: function (e) {
-      console.log(e)
-    },
-    scroll: function (e) {
-      console.log(e)
-    },
-    tap: function (e) {
-      for (var i = 0; i < order.length; ++i) {
-        if (order[i] === this.data.toView) {
-          this.setData({
-            toView: order[i + 1]
-          })
-          break
-        }
-      }
-    },
-    tapMove: function (e) {
-      this.setData({
-        scrollTop: this.data.scrollTop + 10
-      })
-    },
+  // data: {
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+  // },
 
   /**
    * Lifecycle function--Called when page is initially rendered
