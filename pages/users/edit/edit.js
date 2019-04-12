5225e1570b19a9980e52e7e790917c4fccf5c280
Agnes Selvita <a.selvita@gmail.com>
2019-04-12 15:49:21 +0800

    updated user show and user edit

---
+++ pages/users/edit/edit.js
@@ -0,0 +1,114 @@
+// pages/users/edit/edit.js
+const app = getApp();
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
+    console.log(33, userInfo);
+    const page = this;
+    const url = app.globalData.url;
+    const id = app.globalData.userId;
+    const userInfo = app.globalData.userInfo;
+    const avatarUrl = app.globalData.userInfo.avatarUrl;
+    const nickName = userInfo.nickName;
+    console.log(44, userInfo);
+
+    wx.request({
+      url: `${url}users/${id}`,
+      method: "GET",
+      success(res) {
+        console.log(res)
+        const user = res.data;
+        console.log('onload user', user);
+        page.setData(user);
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  formSubmit: function (event) {
+    const page = this;
+    let first_name = event.detail.value.firstName;
+    let last_name = event.detail.value.lastName;
+    let user_id = app.globalData.userId
+    let userDetail = { first_name: first_name, last_name: last_name }
+
+    console.log(first_name)
+    console.log(last_name)
+
+    const url = app.globalData.url;
+    const id = app.globalData.userId;
+
+    wx.request({
+      url: `${url}users/${id}`,
+      method: 'PUT',
+      data: userDetail,
+      success(res) {
+        wx.reLaunch({
+          url: `/pages/users/show/show?id=${id}`
+        });
+      }
+    });
+  },
+})
